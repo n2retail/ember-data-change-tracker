dabe3779280ca0386c2bd9b744629d9b57548f1f
Ryan Tablada <ryan.tablada@gmail.com>
2022-12-15 06:40:53 -0600

    rename initializer deprecation

--- addon/initializer.js
+++ addon/initializer.js
@@ -3,7 +3,7 @@ import { deprecate } from '@ember/debug';
 /* global require */
 export function initializer() {
   deprecate('Do not initialize ember-data-change-tracker', false, {
-    id: 'ember-data-change-tracker.invalid',
+    id: 'ember-data-change-tracker.initializer',
     until: '3.28.0',
     for: 'ember-data-change-tracker'
   });
